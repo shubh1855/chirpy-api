13f1c573c05d072be305eb3d71bb285fa700bc8b
shubh1855 <paulshubhranil1855@gmail.com>
2026-06-11 13:12:30 +0530

    added reqeust metrics and reset endpoints

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add((1))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+	hits := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %d", hits)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset"))
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+
+	apiCfg := &apiConfig{}
 
 	mux := http.NewServeMux()
 
@@ -17,10 +47,15 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
-	mux.Handle("/app/", http.StripPrefix(
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	fileServer := http.StripPrefix(
 		"/app",
 		http.FileServer(http.Dir(filepathRoot)),
-	))
+	)
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
 
 	server := &http.Server{
 		Addr:    ":" + port,
