119dd47797cbc3c5afac54c865ffd04b0810fb87
shubh1855 <paulshubhranil1855@gmail.com>
2026-06-12 13:03:33 +0530

    feat: add chirp creation endpoint

---
+++ chirp.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"httpserver/internal/database"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type createChirpParams struct {
+	Body   string    `json:"body"`
+	UserID uuid.UUID `json:"user_id"`
+}
+
+type parameters struct {
+	Body string `json:"body"`
+}
+
+type Chirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func cleanProfanity(text string) string {
+	badWords := map[string]bool{
+		"kerfuffle": true,
+		"sharbert":  true,
+		"fornax":    true,
+	}
+
+	words := strings.Split(text, " ")
+
+	for i, word := range words {
+		if badWords[strings.ToLower(word)] {
+			words[i] = "****"
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func (cfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request) {
+	var params createChirpParams
+
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		respondWithError(
+			w,
+			http.StatusBadRequest,
+			"Invalid request",
+			err,
+		)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(
+			w,
+			http.StatusBadRequest,
+			"Chirp is too long",
+			nil,
+		)
+		return
+	}
+
+	cleanedBody := cleanProfanity(params.Body)
+
+	chirp, err := cfg.db.CreateChirp(
+		r.Context(),
+		database.CreateChirpParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			Body:      cleanedBody,
+			UserID:    params.UserID,
+		},
+	)
+
+	if err != nil {
+		respondWithError(
+			w,
+			http.StatusInternalServerError,
+			"Couldn't create chirp",
+			err,
+		)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, Chirp{
+		ID:        chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body:      chirp.Body,
+		UserID:    chirp.UserID,
+	})
+}
--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerCreateChirp)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 
 	fileServer := http.StripPrefix(
